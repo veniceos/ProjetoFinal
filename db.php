c619baa762a80866421cf694d015e0758456c2af
veniceos <vinicius.canevari@gmail.com>
2023-06-05 08:28:25 -0300

    primeira at.

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'CRUD-FRUTAS';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,
+    PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Erro ao conectar ao banco de dados: '
+    .$e->getMessage();
+    exit;
+}
+
+?>
